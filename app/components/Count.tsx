e1b4e86c6a064b9df452deb45330f6b8b8964265
timetraveller98 <chuhan718@gmail.com>
2024-07-03 17:42:22 +0530

    css a

--- app/components/Count.tsx
+++ app/components/Count.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useSelector, useDispatch, TypedUseSelectorHook } from "react-redux";
 import { increment, decrement } from "@/app/redux/slice";
 import type { RootState, AppDispatch } from "@/app/redux/store"; // Adjust the import path accordingly
+import Button from '@mui/material/Button';
 
 const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
@@ -13,12 +14,16 @@ const Counter = () => {
   return (
 
 
-    <div>
-      <div> <button onClick={() => dispatch(increment())}> + </button>
-      <span>
+    <div className="d-flex align-items-center justify-content-center mt-5">
+      <div> <Button variant="contained" color="error" size="large" onClick={() => dispatch(increment())}> <span style={{fontSize:"40px"}} className="p-2">
+        +
+      </span></Button>
+      <span style={{fontSize:"40px"}} className="p-4 m-5 text-danger">
         {count}
       </span>
-        <button onClick={() => dispatch(decrement())} > - </button>
+        <Button variant="contained" color="error" size="large" onClick={() => dispatch(decrement())} ><span style={{fontSize:"40px"}} className="p-2">
+        -
+      </span></Button>
       </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "@/app/redux/provider";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const inter = Inter({ subsets: ["latin"] });
